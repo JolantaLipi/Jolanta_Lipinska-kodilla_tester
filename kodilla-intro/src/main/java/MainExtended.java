7365828377f189ff706a249e6caf6b33ba6e806d
Jolanta.Lipińska <gwiazda1906@gmail.com>
2024-11-11 18:33:52 +0100

    HelloWorld

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,15 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Jola" ;
+        char favoriteLetter = 'M' ;
+        double height = 1.70 ;
+        int myAge = 33 ;
+        boolean lovesCookies = true ;
+        String haslo = "Okon" ;
+        System.out.println("myName") ;
+        System.out.println("favoriteLetter") ;
+        System.out.println("myAge") ;
+        System.out.println("lovesCookies") ;
+        System.out.println("haslo") ;
+    }
+}
