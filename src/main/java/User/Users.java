b28dda27a910fbfdc2e3df15113a77dfc590fdc2
Jolanta Lipińska <gwiazda1906@gmail.com>
2024-12-08 12:04:41 +0100

    Initial version

---
+++ src/main/java/User/Users.java
@@ -0,0 +1,13 @@
+package User;
+
+public class Users {
+    public static <User, User> void main(String[] args) {
+        User asia = new User("Asia", 20);
+        User basia = new User("Basia", 33);
+        User czesia = new User("Czesia", 58);
+        User danka = new User("Danka", 13);
+        User ewa = new User("Ewa", 18);
+
+
+    }
+}
