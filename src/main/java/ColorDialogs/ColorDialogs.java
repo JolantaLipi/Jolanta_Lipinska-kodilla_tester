5c6beb4376da7a6274e6f20257a9b4c80595fb5e
Jolanta Lipińska <gwiazda1906@gmail.com>
2024-12-09 15:42:59 +0100

    Initial version

---
+++ src/main/java/ColorDialogs/ColorDialogs.java
@@ -0,0 +1,10 @@
+package ColorDialogs;
+
+public class ColorDialogs {
+    public String getColorName() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("Enter ")
+        }
+    }
+}
---
+++ src/main/java/ColorDialogs/getColorName.java
@@ -0,0 +1,3 @@
+package ColorDialogs;
+
+import java.util.Scanner;
